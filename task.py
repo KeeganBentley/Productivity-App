9632b8ded3915adfb89f83e49769a74a1ff30803
KeeganBentley <keegan.bentley@my.jcu.edu.au>
2017-07-05 12:41:46 +1000

    Initial commit

---
+++ task.py
@@ -0,0 +1,10 @@
+"""
+Class for a task for use in productivity app
+started: 5/07/17 
+Keegan Bentley
+"""
+import random
+
+class Task:
+    def __init__(self, task_no, name, description, due_date='na'):
+        self.task_no =
